a742b077967f2f0129df2c633b11391372de7250
NazarBaloch04 <153612842+NazarBaloch04@users.noreply.github.com>
2024-06-29 02:32:25 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,35 @@
+enum daysOfTheWeek{
+    Sun, Mon, Tue, Wed, Thurs, Fri, Satur
+};
+const dayOfTheWeek = daysOfTheWeek.Mon
+if(daysOfTheWeek === daysOfTheWeek){
+    console.log("its Monday");
+    
+}else{
+    console.log("its not monday");
+    
+}
+let i 
+for(i=0; i>90; i++){
+console.log(`Assalam o Alaikum Brother ${i}`);
+
+}
+
+
+
+
+
+
+enum Students{
+    mazari=12,bilal="bilal",channa="channa",khuram="khuram",fazal="haya"
+}
+const student:Students=Students['fazal']
+console.log(student)
+
+
+
+function f1(p:string){
+    return p
+}
+console.log(f1(Students.fazal))
+
